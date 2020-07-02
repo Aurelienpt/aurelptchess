b3c957737720769f2cff6f6ef68ee738edb9f51e
Aurelien <aurelpt@gmail.com>
2020-07-02 18:52:21 +0200

    Ajout d'un favicon, changement sur la page autre

--- js/fetchchesscom.js
+++ js/fetchchesscom.js
@@ -6,15 +6,24 @@ response.text().then(function(text){
 
         var a = document.getElementById("avatar");
         a.setAttribute('src', texte["avatar"]);
-        document.getElementById("username").append(texte["username"])
+        document.getElementById("username").append(texte["username"]);
         document.getElementById("amis").append(texte["followers"]);
         
 
-        let unix_timestamp = texte["last_online"]
-        var date = new Date(unix_timestamp*1000);
+        // let unix_timestamp = texte["last_online"];
+        // var date = new Date(unix_timestamp*1000);
 
-        document.getElementById("online").append(date);
+        // document.getElementById("online").append(date);
 
     });
 
+});
+
+fetch("https://api.chess.com/pub/player/gamingboiii/stats").then(function(response) {
+    response.text().then(function(text) {
+        texte = JSON.parse(text);
+        document.getElementById("elo").append(texte["chess_blitz"]["last"]["rating"]);
+        document.getElementById('victoire').append(texte["chess_blitz"]["record"]["win"]);
+        document.getElementById('defaite').append(texte["chess_blitz"]["record"]["loss"]);
+    });
 });
